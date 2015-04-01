45f4ce5b7ee4a1f06ceef205d2968711aa92ce68
eric wong <win19999@gmail.com>
2015-04-01 21:24:33 +0800

    fix quick sort test

--- src.basic/org/googlecode/java/thinking/basic/algorithm/QuickSortTest.java
+++ src.basic/org/googlecode/java/thinking/basic/algorithm/QuickSortTest.java
@@ -2,37 +2,55 @@ package org.googlecode.java.thinking.basic.algorithm;
 
 import java.util.Arrays;
 
+import org.googlecode.java.thinking.basic.util.Print;
+import org.junit.Test;
+
 public class QuickSortTest {
 	
+	@Test
 	public void test() {
 		
 		int[] arr = new int[]{4, 6, 3, 89, 54, 454, 242, 1, 3, 98, 56};
-		Arrays.sort(arr);
+		//Arrays.sort(arr);
+		qsort(arr, 0, arr.length-1);
+		Print.print(Arrays.toString(arr));
 	}
 	
-	private int partition(int[] arr, int start, int end) {
-		int partition = (start+end)/2;
+	private void qsort(int[] arr, int start, int end) {
+		if (start > end) {
+			int partition = partition(arr, start, end);
+			qsort(arr, start, partition-1);
+			qsort(arr, partition+1, end);
+		}
 		
-		int pivot = arr[partition];
+	}
+	
+	////优化，可以选择开始，结束，和中间三者中间的数值作为partition
+	private int partition(int[] arr, int start, int end) {
+		
+		int pivotPos = (start+end)/2;
+		
+		int pivot = arr[pivotPos];
+		
+		arr[start] = pivot;
 		
 		int i = start;
 		int j = end;
-		boolean first=true;
-		while (i < j) {
-			while (arr[j] >= pivot) {
+		while (i < j) {//边界如何选择？
+			while (j > i && arr[j] >= pivot) {
 				j--;
 			}
-			if (first) 
-				arr[partition] = arr[j];
-			else
+			if (arr[j] >= pivot)
 				arr[i] = arr[j];
-				
-			while (arr[i] <= pivot) {
+			
+			while (i < j && arr[i] <= pivot) {
 				i++;
 			}
+			if (arr[i] <= pivot)
 			arr[j] = arr[i];
 		}
+		arr[i] = pivot;
 		
-		return partition;
+		return i;
 	}
 }
